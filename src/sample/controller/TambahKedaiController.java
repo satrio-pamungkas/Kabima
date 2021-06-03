64d99847f7e9d1fa8acae3a032a3d6db4cf1d003
Muhammad Raihan Satrio Putra Pamungkas <adewipo@gmail.com>
2021-06-03 10:45:23 +0700

    feat: added insert kedai

--- src/sample/controller/TambahKedaiController.java
+++ src/sample/controller/TambahKedaiController.java
@@ -2,6 +2,7 @@ package sample.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.config.Connect;
@@ -9,8 +10,13 @@ import sample.config.Connect;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class TambahKedaiController {
+    Connect dbConnect = new Connect();
+
     @FXML
     private TextField inputNamaKedai;
 
@@ -20,11 +26,66 @@ public class TambahKedaiController {
     @FXML
     private TextArea inputDeskripsiKedai;
 
+    private Alert informationMsg = new Alert(Alert.AlertType.INFORMATION);
+    private Alert warningMsg = new Alert(Alert.AlertType.WARNING);
+
+    private static final SimpleDateFormat waktu = new SimpleDateFormat("MMddHHmmss");
+
     @FXML
     public void tambahKedaiButton(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         boolean isEmpty = isInputEmpty(inputNamaKedai, inputKategoriKedai, inputDeskripsiKedai);
 
+        if (isEmpty) {
+            warningMsg.setTitle("Perhatian !");
+            warningMsg.setHeaderText(null);
+            warningMsg.setContentText("Input masih ada yang kosong");
+            warningMsg.showAndWait();
+        } else {
+            String kodeKedai = "KD1" + getTimestamp();
+            String namaKedai = inputNamaKedai.getText();
+            String kategoriKedai = inputKategoriKedai.getText();
+            String deskripsiKedai = inputDeskripsiKedai.getText();
 
+            insertToDB(kodeKedai, namaKedai, kategoriKedai, deskripsiKedai);
+
+            informationMsg.setTitle("Informasi");
+            informationMsg.setHeaderText(null);
+            informationMsg.setContentText("Kedai berhasil ditambahkan");
+            informationMsg.showAndWait();
+
+            inputNamaKedai.clear();
+            inputKategoriKedai.clear();
+            inputDeskripsiKedai.clear();
+
+        }
+
+    }
+
+    private void insertToDB(String kode, String nama, String kategori, String deskripsi) {
+        String query = "INSERT INTO Kedai(id_kedai, nama_kedai, kategori, deskripsi) VALUES(?,?,?,?)";
+
+        try (Connection conn = dbConnect.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+            preparedStatement.setString(1, kode);
+            preparedStatement.setString(2, nama);
+            preparedStatement.setString(3, kategori);
+            preparedStatement.setString(4, deskripsi);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private String getTimestamp() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        return waktu.format(timestamp);
     }
 
     private boolean isTextFieldEmpty(TextField textField) {
@@ -38,4 +99,5 @@ public class TambahKedaiController {
     private boolean isInputEmpty(TextField namaKedai, TextField kategoriKedai, TextArea deskripsi) {
         return isTextFieldEmpty(namaKedai) || isTextFieldEmpty(kategoriKedai) || isTextAreaEmpty(deskripsi);
     }
+
 }
