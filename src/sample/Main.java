dbfeaa680a52ac801ae23490987b53e6109d86f0
Muhammad Raihan Satrio Putra Pamungkas <adewipo@gmail.com>
2021-06-03 22:26:15 +0700

    feat: added button css

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("resources/view/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/view/splash.fxml"));
         primaryStage.setTitle("Aplikasi KaBiMa");
         primaryStage.setScene(new Scene(root, 810, 530));
         primaryStage.setResizable(false);
---
+++ src/sample/controller/SplashController.java
@@ -0,0 +1,24 @@
+package sample.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SplashController {
+    @FXML
+    private Button buttonMasuk;
+
+    @FXML
+    public void masukClicked(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage = (Stage) buttonMasuk.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("../resources/view/main.fxml"));
+        primaryStage.getScene().setRoot(newRoot);
+    }
+
+
+}
